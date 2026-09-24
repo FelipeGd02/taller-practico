0c929a7f0dd944562c3f9ff7103e01c6319307fb
FelipeGd02 <felipe.gonzalez.0225@gmail.com>
2026-09-24 18:33:06 -0500

    corrección error en order module

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -3,7 +3,7 @@ import { OrdersService } from './orders.service';
 import { OrdersController } from './orders.controller';
 import { OrderEntity } from './entities/order.entity';
 import { CustomerEntity } from './entities/customer.entity';
-import { TypeOrmModule } from 'node_modules/@nestjs/typeorm/dist/typeorm.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { DiningEntity } from './entities/dining.entity';
 import { OrderRulesService } from './order-rules/order-rules.service';
 import { OrderPreparationEstimateService } from './order-preparation-estimate/order-preparation-estimate.service';
